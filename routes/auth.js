4fb52dcbe024ed8d0da03555aaff4f88f6a2125e
Nshuti Maurice <nshutikope@gmail.com>
2025-12-14 19:09:41 +0200

    Fix authentication middleware and routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,14 @@
-import { registerUser, loginUser, logoutUser } from '../controllers/authController.js'
-import express from 'express'
+/** @format */
 
-export const authRouter = express.Router()
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+} from "../controllers/authController.js";
+import express from "express";
 
-authRouter.post('/register', registerUser)
-authRouter.post('/login', loginUser)
-authRouter.get('/logout', logoutUser)
+export const authRouter = express.Router();
+
+authRouter.post("/register", registerUser);
+authRouter.post("/login", loginUser);
+authRouter.get("/logout", logoutUser);
